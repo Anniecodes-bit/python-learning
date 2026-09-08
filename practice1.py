e40a678e6d1d22acbd8b887fa50eddc1344cd744
Anniecodes-bit <anniesgleaming@gmail.com>
2026-09-08 18:27:29 +0530

    Add files via upload

---
+++ practice1.py
@@ -0,0 +1,5 @@
+# program to take as input print value entered and its data  type
+
+age=input("enter your age:")
+print("value entered is:",age)
+print("Data type is :",type(age))
---
+++ userinput.py
@@ -0,0 +1,8 @@
+# program to take input from the user
+
+
+#print("A sample program to understand input method in python")
+
+name= input("enter your goood name:")
+Age=17
+print("your good name is:",name)
